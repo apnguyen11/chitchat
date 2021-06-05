2f8418ae8d805fc88ee8b2314974e6ead3523a53
user <user@domain.com>
2021-06-05 13:50:37 -0500

    enable request logging.

--- server/server.go
+++ server/server.go
@@ -19,13 +19,19 @@ func main() {
 	// Set routing rules
 	http.HandleFunc("/messages/send", SendMessage)
 	http.HandleFunc("/messages/receive", GetMessage)
-	
 
 	//Use the default DefaultServeMux.
-	err := http.ListenAndServe(":8080", nil)
+	err := http.ListenAndServe(":8080", logRequest(http.DefaultServeMux))
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func logRequest(handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("%s %s %s\n", r.RemoteAddr, r.Method, r.URL)
+		handler.ServeHTTP(w, r)
+	})
 }
 
 func enableCors(w *http.ResponseWriter) {
