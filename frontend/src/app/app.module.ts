66a76c5c81e2504ee85bf41a24173dadb29af96b
Andy Nguyen <apnguyen11@yahoo.com>
2021-06-14 20:54:36 -0500

    Channel component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ChannelsComponent } from './channels/channels.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChannelsComponent
   ],
   imports: [
     BrowserModule,
